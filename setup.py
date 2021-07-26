9b6348f61a2ed171d10380614c1dfcba44c119e8
Joris Vincent <joris.vincent@gmail.com>
2021-07-26 10:08:44 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='domijan2015',
-      version='0.1',
-      description='Domijan2015',
-      author='Lynn Schmittwilken',
-      author_email='l.schmittwilken@tu-berlin.de',
-      license='MIT',
-      packages=['domijan2015'],
-      zip_safe=False)
+setup(
+    name="domijan2015",
+    version="0.1",
+    description="Domijan2015",
+    author="Lynn Schmittwilken",
+    author_email="l.schmittwilken@tu-berlin.de",
+    license="MIT",
+    packages=["domijan2015"],
+    zip_safe=False,
+    install_requires=["scipy", "numpy", "matplotlib", "pillow"],
+)
